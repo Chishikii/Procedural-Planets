0e6eb2164f32bc23c9949cb681be8b80640200cb
finnpelzer <finn@pelzers.com>
2019-09-29 21:36:05 +0200

    Used for generating the planets mesh

--- Assets/Scenes/TerrainFace.cs
+++ Assets/Scenes/TerrainFace.cs
@@ -11,23 +11,59 @@ public class TerrainFace
     // other 2 vectors
     Vector3 axisA;
     Vector3 axisB;
-
+    
     public TerrainFace(Mesh mesh, int resolution, Vector3 localUp)
     {
         this.mesh = mesh;
         this.resolution = resolution;
         this.localUp = localUp;
 
-        axisA = new Vector3(localUp.y, localUp.x, localUp.z);
+        axisA = new Vector3(localUp.y, localUp.z, localUp.x);
         axisB = Vector3.Cross(localUp, axisA);
     }
 
-    public void ConstructMesh()
-    {
-        //All the vertices for the polys 
-        Vector3[] vertices = new Vector3[resolution * resolution];
-        //vertices for the triangles
-        int[] triangles = new int[(resolution - 1) * (resolution - 1) * 2 * 3];
+	public void ConstructMesh()
+	{
+		//All the vertices for the polys 
+		Vector3[] vertices = new Vector3[resolution * resolution];
+		//vertices for the triangles needs index % 3 = 0
+		int[] triangles = new int[(resolution - 1) * (resolution - 1) * 2 * 3];
+		int triIndex = 0;
 
-    }
+		for (int y = 0; y < resolution; y++)
+		{
+			for (int x = 0; x < resolution; x++)
+			{
+				int i = x + y * resolution;
+				//essentially just tells how far the 2 loops have progressed from 0 to 1
+				Vector2 percent = new Vector2(x, y) / (resolution - 1);
+
+                Vector3 pointOnUnitCube = localUp + (percent.x - .5f) * 2 * axisA + (percent.y - .5f) * 2 * axisB;
+                Vector3 pointOnUnitSphere = pointOnUnitCube.normalized;
+				vertices[i] = pointOnUnitSphere;
+
+				//definining vertices clockwise
+				//not the outer edge (right) and lower edge (bottom) since outside mesh
+				if (x != resolution - 1 && y != resolution - 1)
+				{
+					//first triangle
+					triangles[triIndex] = i;
+					triangles[triIndex + 1] = i + resolution + 1;
+					triangles[triIndex + 2] = i + resolution;
+
+					//triangle 2
+					triangles[triIndex + 3] = i;
+					triangles[triIndex + 4] = i + 1;
+					triangles[triIndex + 5] = i + resolution + 1;
+
+					triIndex += 6;
+				}
+			}
+		}
+
+        mesh.Clear();
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+        mesh.RecalculateNormals(); 
+	}
 }
